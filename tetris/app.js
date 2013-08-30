f73ffde4d483ca504b640271df95f16b9348b94f
Rodrigo Silveira <rsilveira@deseretdigital.com>
2013-08-30 17:34:48 -0600

    added gameloop

--- tetris/app.js
+++ tetris/app.js
@@ -37,11 +37,14 @@ var Piece = function(x, y) {
 	};
 
 	var size = {
-		width : 2,
-		height : 2
+		width : 3,
+		height : 3
 	};
 
-	var shape = [ true, true, true, true ];
+	var shape = [
+         0, 1, 0,
+         1, 1, 0
+     ];
 
 	this.getPos = function() {
 		return pos;
@@ -59,18 +62,42 @@ var Piece = function(x, y) {
 		pos.x += x || 0;
 		pos.y += y || 0;
 	};
+
+	this.getOffset = function() {
+		var max = {
+			x: 0,
+			y: 0
+		};
+		
+		for (var i = 0, len = shape.length; i < len; i++) {
+			if (shape[i]) {
+				var x = i % size.width;
+				var y = parseInt(i / size.height);
+				if (x > max.x) {
+					max.x = x;
+				}
+				
+				if (y > max.y) {
+					max.y = y;
+				}
+			}
+		}
+		
+		return max;
+	};
 };
 
-var Board = function(rows, cols, width, height) {
+var Board = function(cols, rows, width, height) {
 	var cell = {
-		width : width || 16,
-		height : height || 16
+		width : width || 32,
+		height : height || width || 32
 	};
+
 	var grid = {
 		rows : rows || 10,
 		cols : cols || 10
 	};
-
+this.getGrid = function() { return grid; };
 	var piece = void 0;
 
 	this.setPiece = function(pPiece) {
@@ -82,24 +109,44 @@ var Board = function(rows, cols, width, height) {
 			return;
 		}
 
+		var pos = piece.getPos();
+		var offset = piece.getOffset();
+		
 		if (KEYS[KEY_MAPPING.LEFT_KEY]) {
-			piece.moveBy(-1, 0);
+			if (pos.x > 0) {
+				piece.moveBy(-1, 0);
+			}
 		}
 		
 		if (KEYS[KEY_MAPPING.RIGHT_KEY]) {
-			piece.moveBy(1, 0);
+			if (pos.x + offset.x < grid.cols - 1) {
+				piece.moveBy(1, 0);
+			}
 		}
 		
 		if (KEYS[KEY_MAPPING.DOWN_KEY]) {
-			piece.moveBy(0, 1);
+			if (pos.y + offset.y < grid.rows - 1) {
+				piece.moveBy(0, 1);
+			}
+		}
+		
+		if (KEYS[KEY_MAPPING.UP_KEY]) {
+			piece.moveBy(0, -1);
 		}
 	};
 	
 	this.draw = function(canvas) {
 		var pos = piece.getPos();
+		var size = piece.getSize();
 		var shape = piece.getShape();
 
-		canvas.draw(pos.x * grid.cols, pos.y * grid.rows, cell.width, cell.height);
+		for (var i = 0, len = shape.length; i < len; i++) {
+			if (shape[i]) {
+				var x = i % size.width;
+				var y = parseInt(i / size.height);
+				canvas.draw(pos.x * cell.width + x * cell.width, pos.y * cell.height + y * cell.height, cell.width, cell.height);
+			}
+		}
 	};
 };
 
